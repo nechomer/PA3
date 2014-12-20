b2f05a38c4dc06844cc62da8021417718e407289
roeyasher <roey.asher@gmail.com>
2014-12-20 17:26:43 +0200

    update

--- PA3 test/test.py
+++ PA3 test/test.py
@@ -7,7 +7,7 @@ ProgFileList = glob("input/*.ic")
 for Progfile in ProgFileList:
 	fname = str(Progfile).split('\\')[-1:][0]
 	f1 = open('output/'+fname+'.out', 'w+')
-	args = ['PA3.jar', Progfile]
+	args = ['PA3.jar', Progfile, "-dump-symtab"]
 	process = Popen(['java', '-jar']+list(args), stdout=f1, stderr=f1)
 	f1.close
 	
@@ -15,7 +15,7 @@ ProgFileList = glob("input/including_Library/*.ic")
 for Progfile in ProgFileList:
 	fname = str(Progfile).split('\\')[-1:][0]
 	f1 = open('output/'+fname+'.out', 'w+')
-	args = ['PA3.jar', Progfile,"-Linput/including_Library/libic.sig"]
+	args = ['PA3.jar', Progfile,"-Linput/including_Library/libic.sig", "-dump-symtab"]
 	process = Popen(['java', '-jar']+list(args), stdout=f1, stderr=f1)
 	f1.close
 

bb5d52c365339ea3d53fc80558c0ed5e03b87edd
roeyasher <roey.asher@gmail.com>
2014-12-20 17:02:57 +0200

    update test py

--- PA3 test/test.py
+++ PA3 test/test.py
@@ -3,19 +3,19 @@ import sys
 from glob import glob
 from subprocess import *
 
-ProgFileList = glob("pa-3-input/*.ic")
+ProgFileList = glob("input/*.ic")
 for Progfile in ProgFileList:
 	fname = str(Progfile).split('\\')[-1:][0]
-	f1 = open('/pa-3-output/'+fname+'.out', 'w+')
+	f1 = open('/output/'+fname+'.out', 'w+')
 	args = ['CompilationPA3_fat.jar', Progfile, -dump-symtab]
 	process = Popen(['java', '-jar']+list(args), stdout=f1, stderr=f1)
 	f1.close
 	
-ProgFileList = glob("pa-3-input/including_Library/*.ic")
+ProgFileList = glob("input/including_Library/*.ic")
 for Progfile in ProgFileList:
 	fname = str(Progfile).split('\\')[-1:][0]
-	f1 = open('pa-3-output/'+fname+'.out', 'w+')
-	args = ['CompilationPA3a_fat.jar', Progfile,"-LC:/Users/Roey/Desktop/PA3 test/pa-3-input/lib/libic.sig", -dump-symtab]
+	f1 = open('output/'+fname+'.out', 'w+')
+	args = ['CompilationPA3a_fat.jar', Progfile,"-Lincluding_Library/libic.sig", -dump-symtab]
 	process = Popen(['java', '-jar']+list(args), stdout=f1, stderr=f1)
 	f1.close
 
